cd6da2c3364fc34ac3b1345fe8627f69f0c96b25
Andrea Shirazi Sotomayor <andrea.shirazi@ocsbstudent.ca>
2020-03-02 14:29:01 -0500

    MovingImages

--- AreaRectCircleAndrea/main.lua
+++ AreaRectCircleAndrea/main.lua
@@ -12,47 +12,84 @@ local myRectangle
 local widthOfRectangle = 350
 local heightOfRectangle = 200
 local areaOfRectangle
+local textAreaCircle
+local myCircle
+local areaCircle
+local radiusCircle = 100
+local PI = 3.14
 
--- set the background colour of my screen. Remember that colours are between 0
---and 1.
+-- set the background colour of my screen. Remember that colours are between 0 and 1.
 display.setDefault("background", 28/255, 121/255, 243/255)
 
---to remove status bar
+-- to remove status bar
 display.setStatusBar(display.HiddenStatusBar)
 
 --draw the rectangle that is half the width and height of the screen size.
 myRectangle = display.newRect(0,0, widthOfRectangle, heightOfRectangle)
 
---anchor the rectangle in the top left corner of the screen and set its (x,y)
---position
+-- anchor the rectangle in the top left corner of the screen and set its (x,y) position
 myRectangle.anchorX = 0
 myRectangle.anchorY = 0
 myRectangle.x = 20
 myRectangle.y = 20
 
---set the width of the border
+-- set the width of the border
 myRectangle.strokeWidth = 20
 
---set the colour of the rectangle
+-- set the colour of the rectangle
 myRectangle:setFillColor(0.7, 0.1, 0.3)
 
---set the colour of the border
+-- set the colour of the border
 myRectangle:setStrokeColor(0, 1, 0)
 
---calculate the area
+-- calculate the area
 areaOfRectangle = widthOfRectangle * heightOfRectangle
 
---write the area on the screen. Take into consideration the size of the font
---when positioning it on the screen
-areaText=display.newText("The area of this rectangle with a width of \n" ..
+-- write the area on the screen. Take into consideration the size of the font when positioning it on the screen
+areaText = display.newText("The area of this rectangle with a width of \n" ..
 	widthOfRectangle .. " and a height of " .. heightOfRectangle .. " is " ..
 	areaOfRectangle .. " pixels².", 0, 0, Arial, textSize)
 
---anchor the text and set its (x,y) position
+-- anchor the text and set its (x,y) position
 areaText.anchorX = 0
 areaText.anchorY = 0
 areaText.x = 20
 areaText.y = display.contentHeight/2
 
---set the colour of the newText
+-- set the colour of the newText
+areaText:setTextColor(1, 1, 1)
+
+-- draw the circle that is a quarter of the screens radius
+myCircle = display.newCircle(520, 600, radiusCircle, PI)
+-- anchor the circle in the bottom left corner of the screen and set its (x,y) position
+myCircle.anchorX = 0
+myCircle.anchorY = 0
+myCircle.x = 630
+myCircle.y = 20
+
+-- set the width of the border
+myCircle.strokeWidth = 20
+
+-- set the colour of the rectangle
+myCircle:setFillColor(0.7, 0.1, 0.3)
+
+-- set the colour of the border
+myCircle:setStrokeColor(0, 1, 0)
+
+-- calculate the area
+areaCircle = radiusCircle * PI * radiusCircle
+
+-- write the area on the screen. Take into consideration the size of the font
+-- when positioning it on the screen
+textAreaCircle=display.newText("The area of this circle with a radius of \n" ..
+	radiusCircle .. " and PI " .. PI .. " is " ..
+	areaCircle .. " pixels².", 700, 800, Arial, textSize)
+
+-- anchor the text and set its (x,y) position
+textAreaCircle.anchorX = -20
+textAreaCircle.anchorY = 40
+textAreaCircle.x = 25
+textAreaCircle.y = display.contentHeight/2
+
+-- set the colour of the newText
 areaText:setTextColor(1, 1, 1)
---
+++ MovingImagesAndreaS/main.lua
@@ -0,0 +1,56 @@
+-- Title: MovingObjects
+-- Name: Andrea Shirazi
+-- Course: ICS2O/3C
+-- This program moves a beetleship across the screen and
+-- then makes it fade out. I added a second object that 
+-- moves in a different direction, fades in and grows in size.
+
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- global variables
+scrollSpeed = 3 
+
+-- background image with width and height
+local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
+
+-- character image with width and height
+local beetleship = display.newImageRect("Images/beetleship.png", 200, 200)
+
+-- set the image to be transparent 
+beetleship.alpha = 0
+
+-- set the intial x and y position of beetleship
+beetleship.x = 0 
+beetleship.y = display.contentHeight/3
+
+-- Function: MoveShip
+-- Input: This function accepts an event listener 
+-- Output: none
+-- Description: This function adds the scroll speed to the x-value 
+-- of the ship
+local function MoveShip(event)
+	--add the scroll speed to the x-value of the ship
+	beetleship.x = beetleship.x + scrollSpeed
+	-- change the transparency of the ship every time it moves
+	-- so that it fades out 
+	beetleship.alpha = beetleship.alpha + 0.01
+end
+
+-- MoveShip will be called over and over again
+Runtime:addEventListener("enterFrame", MoveShip)
+
+-- character image with width and height
+local rocketship = display.newImageRect("Images/rocketship.png", 200, 200)
+
+-- set the image to be transparent
+rocketship.alpha = 1
+
+-- set the inital x and y position of rocketship
+rocketship.x = 1024
+rocketship.y = display.contentHeight/2
+
+-- flip the image horizontally
+rocketship:scale (-1, 1)
+
+-- function: MoveShip
