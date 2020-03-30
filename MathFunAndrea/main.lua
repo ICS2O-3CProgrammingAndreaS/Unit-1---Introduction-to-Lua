3c6c3b85dc94ce195019e69345f67ceac2f43600
Andrea Shirazi Sotomayor <61516895+s460623@users.noreply.github.com>
2020-03-29 21:20:41 -0400

    Update main.lua

--- MathFunAndrea/main.lua
+++ MathFunAndrea/main.lua
@@ -1,7 +1,118 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: NumericTextFields
+-- Name: Andrea Shirazi
+-- Course: ICS2O/3C
+-- This program
 
--- Your code here
+----
+
+-- hide the statu bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- set the default background
+display.setDefault("Background" 230/255, 145/255, 243/255)
+
+
+-- 
+--local variables
+--
+local qobject
+local cobject
+local NumericTextFields
+local randomnumber1
+local randomnumber2
+local userAns
+local cAns
+local incAns
+local randomOp
+local incobject
+
+--
+--local functions
+--
+
+local function askQuestion()
+	--generate a random number between 1 and 4
+	randomOp = math.random(1,4)
+
+	randomNumber1 = math.random(0,10)
+	randomnumber2 = math.random(0,10)
+
+	if (randomOp == 1) then
+		cAns = randomnumber1 + randomnumber2
+
+		qobject.text = randomnumber1 .. " + " .. randomnumber2 .. " = "
+
+	elseif(randomOp == 2) then
+		cAns = randomnumber1 * randomnumber2
+
+		qobject.text = randomnumber1 .. " - " .. randomnumber2 .. " = "
+
+	elseif (randomOp == 3)
+		cAns = randomnumber1 * randomnumber2
+
+		qobject.text = randomnumber1 .. " * " .. randomnumber2 .. " = ""
+
+	elseif (rendomOp == 4) then
+		cAns = randomnumber1/randomnumber2
+
+		cAns = cans * 100
+		math.round(cAns)
+
+		qobject.text = randomnumber1 .. "/" .. randomnumber2 .. " = "
+	end
+end
+
+local function HideCorrect ()
+	cobject.isVisible = false
+	askQuestion()
+end
+
+local function HideIncorrect ()
+	incobject.isVisible = false
+	askQuestion()
+end
+
+local function NumericFieldListener( event )
+	if (event.phase == "began" ) then
+
+		event.target.text = ""
+
+	elseif (event.phase == "submitted") then
+
+		userAns = tonumber(event.target.text)
+
+		if (userAns == cAns) then 
+			cobject.isVisible = true
+			timer.performWithdelay(3000, HideCorrect)
+			correctSoundChannel = audio.play(correctSoundChannel)
+
+		else
+			incobject.isVisible = trie
+			timer.performWithdelay(3000, HideIncorrect)
+			incorrectSoundChannel = audio.play(IncorrectSoundChannel)
+		end
+
+		event.target.text = ""
+	end
+end
+
+-- 
+--object creation
+-- 
+
+qobject = display.newText ( "", display.contentWdith/2, display.contentHeight/2 nil, 70)
+qobject:setTextColor(214/255, 185/255, 15/255)
+
+cobject = display.newText ( "correct!", display.contentWdith/2, display.contentHeight*1/3, nil, 50)
+cobject:setTextColor(214/255, 185/255, 15/255)
+cobject.isVisible = false
+
+
+incobject = display.newText ( "Incorrect!", display.contentWdith/2, display.contentHeight*1/3, nil, 50)
+incobject:setTextColor(214/255, 185/255, 15/255)
+incobject.isVisible = false
+
+numericField = native.newTextField (display.contentHeight*2/3, display.contentWdith/2, 200, 100)
+numericField,inputType = "number"
+
+askQuestion()
